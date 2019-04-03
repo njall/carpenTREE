00e5f215a02b78029ea836cabca44c09b385ef49
Louise Bowler <LouiseABowler@users.noreply.github.com>
2019-04-03 13:54:24 +0100

    Get list of episode urls for each lesson

--- fertilizer.py
+++ fertilizer.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import (
-    urlopen, urlparse, urlunparse, urlretrieve)
+    urlopen, urlparse, urlunparse, urlretrieve, urljoin)
 import os
 import sys
 
@@ -12,6 +12,23 @@ def extract(url):
         if (link):
             keyword_array.append(link.text)
     return keyword_array
+
+def extract_episode_urls(url):
+    soup = bs(urlopen(url), features="lxml")
+    episodes = []
+
+    for table_row in soup.findAll("tr"):
+
+        # See if this table row contains a link - if it is, it will go to an episode of this lesson
+        link = table_row.find('a')
+        if link:
+            relative_url = table_row.find('a').get('href')
+            absolute_url = urljoin(url, relative_url)
+            episodes.append({"url": absolute_url, "name": link.text})
+            print(episodes[-1])
+
+    return episodes
+
 
 def _usage():
     print("usage: python fertilizer.py")
@@ -25,15 +42,30 @@ if __name__ == "__main__":
         'http://swcarpentry.github.io/r-novice-inflammation/reference',
         'http://swcarpentry.github.io/r-novice-gapminder/reference',    
     ]
+
     data_index = {}
     for url in urls:
+        episode_list = extract_episode_urls(url)
+
+
+    for episode in episode_list:
         data_index[url] = extract(url)
+
+
+    # print(data_index)
 
     target_lesson = 'http://swcarpentry.github.io/python-novice-gapminder/13-looping-data-sets/index.html'
     soup = bs(urlopen(target_lesson), features="lxml")
-    prerequisite_lessons = []
-    for lesson_url, lesson_content in data_index.items():
-        for content in lesson_content:
-            if content in soup.text:
-                prerequisite_lessons.append(lesson_url)
-    print(set(prerequisite_lessons))
+
+    prerequisites = []
+
+    for lesson_url, episode_titles in data_index.items():
+        for episode in episode_titles:
+            if episode in soup.text:
+
+                prerequisites.append({"name": episode, "description": episode, "id": lesson_url})
+
+
+    print(prerequisites)
+
+    # print(set(lesson_content))
