6043395f08bcd4a4ca5b18fe3dc83dc0610d7207
Louise Bowler <LouiseABowler@users.noreply.github.com>
2019-04-03 09:41:53 +0100

    Generate scraper

---
+++ treescraper/treescraper/spiders/fertiliser.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class FertiliserSpider(scrapy.Spider):
+    name = 'fertiliser'
+    allowed_domains = ['http://swcarpentry.github.io/r-novice-gapminder/reference']
+    start_urls = ['http://http://swcarpentry.github.io/r-novice-gapminder/reference/']
+
+    def parse(self, response):
+        pass
